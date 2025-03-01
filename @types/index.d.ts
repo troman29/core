0115f1b7932b833a4606f5bc940975407cab8b75
troman29 <mc-monstr@yandex.ru>
2025-03-01 11:40:12 +0300

    Add types

---
+++ @types/index.d.ts
@@ -0,0 +1,8 @@
+type AnyLiteral = Record<string, any>;
+type AnyClass = new (...args: any[]) => any;
+type AnyFunction = (...args: any[]) => any;
+type AnyAsyncFunction = (...args: any[]) => Promise<any>;
+type AnyToVoidFunction = (...args: any[]) => void;
+type NoneToVoidFunction = () => void;
+type ValueOf<T> = T[keyof T];
+type Maybe<T> = T | null;
